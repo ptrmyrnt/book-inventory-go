5ca563c7f6676c316eb6ff20a771fd5878c0b3f8
ptrmyrnt <femiliapm29@gmail.com>
2024-03-06 13:47:48 +0700

    home and main

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"book-inventory-go/app"
+	"book-inventory-go/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	conn := db.InitDB()
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	handler := app.New(conn)
+
+	// home
+	router.GET("/books", handler.GetBooks)
+
+	router.Run()
+}
